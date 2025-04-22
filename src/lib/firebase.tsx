716a7776f26a2cfe3bac252f374c3698a3f95f46
Chinmay Patil <145786800+Chinmay-P-Patil@users.noreply.github.com>
2025-04-23 01:10:44 +0530

    security

--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -8,14 +8,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjePFVlme3vzAgtSmRqkKxngQ3n8CvjiQ",
-  authDomain: "rtoauto-ac8b8.firebaseapp.com",
-  databaseURL: "https://rtoauto-ac8b8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "rtoauto-ac8b8",
-  storageBucket: "rtoauto-ac8b8.firebasestorage.app",
-  messagingSenderId: "474488101509",
-  appId: "1:474488101509:web:23a49775036caac93e27ec",
-  measurementId: "G-F3HSENZFVX"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
